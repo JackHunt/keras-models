ae46f876c0b9d77621eaacfa8c1138a3e9f93852
Jack Hunt <jackmileshunt@gmail.com>
2024-07-06 11:24:46 +0100

    Remove unused import

--- models_lib/utils/data.py
+++ models_lib/utils/data.py
@@ -28,7 +28,6 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from pathlib import Path
 from typing import Tuple
 
 import tensorflow as tf
